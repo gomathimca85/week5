bcac69d005a38db845e0cb40014f2539b861ed51
gomathimca85 <gomathi.r@kgisliim.ac.in>
2023-11-24 15:52:46 +0530

    goms changed

--- main.tf
+++ main.tf
@@ -1,9 +1,31 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-gomathi-project-1234"
+  name     = "test-bucket-goms-project-1234"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-gomathi-project-5678"
+  name     = "test-bucket-goms-project-5678"
   location = "europe-west2"
 }
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-Gomathi-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-Gomathi-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
